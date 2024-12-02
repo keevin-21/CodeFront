6ba175585c791da8d779c31e61735550e56190dd
keevin-21 <kevinaprox123@gmail.com>
2024-12-02 15:49:47 -0800

    CodeFront: A Tech News Webpage.

---
+++ src/main/java/com/java/codeFront/CodeFrontApplication.java
@@ -0,0 +1,16 @@
+package com.java.codeFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class CodeFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFrontApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/java/codeFront/CodeFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.codeFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
